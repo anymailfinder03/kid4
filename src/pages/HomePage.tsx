ae89c1bb6fee3d033a904df4c10682cbc5b9fdd8
lekey1512 <lekey1512@gmail.com>
2026-08-31 20:17:12 +0700

    Add files via upload

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import Hero from '@/components/home/Hero';
+import WhyILE from '@/components/home/WhyILE';
+import Programs from '@/components/home/Programs';
+import LearningJourney from '@/components/home/LearningJourney';
+import Teachers from '@/components/home/Teachers';
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <WhyILE />
+      <Programs />
+      <LearningJourney />
+      <Teachers />
+    </>
+  );
+}
